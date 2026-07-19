19d61b1c7247d82c727340b94ef027990023500c
Mobina Taherifar <taherifar.dev@gmail.com>
2026-07-19 16:18:23 +0330

    feat: implement gameboard logic

---
+++ src/game/Gameboard.js
@@ -0,0 +1,34 @@
+export function createGameBoard(){
+    let ships = [];
+    let shipPositions = new Map();
+    let attackedPositions = new Set();
+
+    return{
+        placeShip(ship, positions){
+            ships.push(ship);
+            positions.forEach(position => {
+                shipPositions.set(position, ship);
+            });
+        },
+
+        receiveAttack(position){
+            if(attackedPositions.has(position)){
+                return "Already attacked";
+            }
+
+            attackedPositions.add(position);
+
+            const ship = shipPositions.get(position);
+            if(ship){
+                ship.hit()
+                return "Hit!"
+            }else{
+                return "Miss!"
+            }
+        },
+
+        allShipsSunk(){
+            return ships.every(ship => ship.isSunk());
+        }
+    }
+}
